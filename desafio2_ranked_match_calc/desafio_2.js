3c2aa3f1dded3cd9b9f6c58800f94819dab18526
Clayton-Dias <crad13@hotmail.com>
2024-11-03 18:48:24 -0300

    Desafio 2
    Ler o txt.

---
+++ desafio2_ranked_match_calc/desafio_2.js
@@ -0,0 +1,44 @@
+// Função para calcular o nível baseado nas vitórias
+function calcularNivel(win, lose) {
+
+    const saldoVitorias = win - lose;
+    
+    let rank;
+
+    switch (true) {
+        case (win < 10):
+            rank = 'Ferro';
+            break;
+        case (win >= 11 && win <= 20):
+            rank = 'Bronze';
+            break;
+        case (win >= 21 && win <= 50):
+            rank = 'Prata';
+            break;
+        case (win >= 51 && win <= 80):
+            rank = 'Ouro';
+            break;
+        case (win >= 81 && win <= 90):
+            rank = 'Diamante';
+            break;
+        case (win >= 91 && win <= 100):
+            rank = 'Lendário';
+            break;
+        case (win >= 101):
+            rank = 'Imortal';
+            break;
+        default:
+            rank = 'Desconhecido';
+    }
+
+    return { saldoVitorias, rank };
+}
+
+// Exemplo de uso
+const win = 75;  
+const lose = 30;
+
+let resultado = calcularNivel(win, lose);
+
+console.log(`O Herói tem de saldo de ${resultado.saldoVitorias} está no nível de ${resultado.rank}!`);
+
